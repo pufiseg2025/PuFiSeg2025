fd427c0cc41f98755edaf8c6058db0f8f2b74ab3
pufiseg2025 <pufiseg2025@gmail.com>
2025-04-14 16:22:32 +0100

    rm FID metric

--- Metrics/task2_eval.py
+++ Metrics/task2_eval.py
@@ -14,7 +14,7 @@ import torch
 import lpips
 from tqdm import tqdm
 import json
-from dists import DISTS
+
 
 EPSILON = 1e-8
 
@@ -22,30 +22,32 @@ EPSILON = 1e-8
 def extract_features(volume):
     return volume.reshape(volume.shape[0], -1)
 
-def normalize_volume(vol):
-    return (vol - vol.min()) / (vol.max() - vol.min() + EPSILON)
+def normalize_volume(vol, min_val=-1024, max_val=3071):
+    return (vol - min_val) / (max_val - min_val + EPSILON)
+
 
 # main
-submit_dir = os.path.join(sys.argv[1], 'res')
-gt_dir = os.path.join(sys.argv[1], 'ref')
-output_dir = sys.argv[2]
+submit_dir = os.path.join(sys.argv[1])
+gt_dir = os.path.join(sys.argv[2])
+output_dir = sys.argv[3]
+
 
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 lpips_model = lpips.LPIPS(net='vgg')
-dists_model = DISTS().eval()
 
-psnr_list, ssim_list, lpips_list, dists_list = [], [], [], []
-act1_list, act2_list = [], []
 
-fids = sorted(glob.glob(gt_dir + "/*.nii.gz"))
+psnr_list, ssim_list, lpips_list = [], [], []
 
+fids = sorted(glob.glob(gt_dir + "/*.nii.gz"))[40:]
+print(f"Found {len(fids)} files in {gt_dir}")
 for gt_path in tqdm(fids):
     fname = os.path.basename(gt_path)
-    pred_path = os.path.join(submit_dir, fname)
+    pred_path = os.path.join(submit_dir, 'LD_'+fname)
 
     if not os.path.exists(pred_path):
+        print(pred_path)
         raise FileNotFoundError(f"Prediction file {fname} not found in submission.")
 
     gt_img = sitk.GetArrayFromImage(sitk.ReadImage(gt_path)).astype(np.float32)
@@ -57,6 +59,7 @@ for gt_path in tqdm(fids):
     psnr_vol, ssim_vol, lpips_vol, dists_vol = [], [], [], []
 
     for i in range(gt_img.shape[0]):
+        print(f"Processing slice {i+1}/{gt_img.shape[0]} in {fname}")
         gt_slice = normalize_volume(gt_img[i])
         pred_slice = normalize_volume(pred_img[i])
 
@@ -67,36 +70,28 @@ for gt_path in tqdm(fids):
         pred_tensor = torch.from_numpy(pred_slice[None, None, :, :]).repeat(1, 3, 1, 1).float()
 
         lpips_val = lpips_model(gt_tensor, pred_tensor).item()
-        dists_val = dists_model(gt_tensor, pred_tensor).item()
+  
 
         psnr_vol.append(psnr_val)
         ssim_vol.append(ssim_val)
         lpips_vol.append(lpips_val)
-        dists_vol.append(dists_val)
+  
 
     psnr_list.append(np.mean(psnr_vol))
     ssim_list.append(np.mean(ssim_vol))
     lpips_list.append(np.mean(lpips_vol))
-    dists_list.append(np.mean(dists_vol))
-
-    act1_list.append(extract_features(gt_img))
-    act2_list.append(extract_features(pred_img))
-
-act1_all = np.concatenate(act1_list, axis=0)
-act2_all = np.concatenate(act2_list, axis=0)
 
 psnr_avg = np.mean(psnr_list)
 ssim_avg = np.mean(ssim_list)
 lpips_avg = np.mean(lpips_list)
-dists_avg = np.mean(dists_list)
+
 
 # Output the average value of each metric
 result_dict = {
     "psnr": round(psnr_avg, 4),
     "ssim": round(ssim_avg, 4),
-    "fid": 0.0, # please evaluate this metric by the compute_fid_2-5d_ct.py file yourself
     "lpips": round(lpips_avg, 4),
-    "dists": round(dists_avg, 4)
+ 
 }
 
 with open(os.path.join(output_dir, "scores.txt"), "w") as f:
